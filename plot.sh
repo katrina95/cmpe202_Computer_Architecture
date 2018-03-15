210f3715424d6266e69e61bdb73071869ca8c890
katrina95 <34797724+katrina95@users.noreply.github.com>
2018-03-15 00:00:21 -0700

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -13,5 +13,5 @@ case $arg in
 	;;
         esac
 done
-/root/pmu-tools/toplev.py -I 1000 -l3 -x, -o tl-build.csv ~/cmpe202_Computer_Architecture/run.sh -i ${instructions} -t ${thread}
-/root/pmu-tools/tl-barplot.py --cpu C0-T0 tl-build.csv -o ~/plot_${instruction}_${thread}.png
+/root/pmu-tools/toplev.py -I 1000 -l3 -x, -o tl-build.csv /root/cmpe202_Computer_Architecture/run.sh -i ${instructions} -t ${thread}
+/root/pmu-tools/tl-barplot.py --cpu C0-T0 tl-build.csv -o /root/plot_${instruction}_${thread}.png
