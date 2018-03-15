170adacd1a7abacc9dc16932993e8069fdfe5b55
katrina95 <34797724+katrina95@users.noreply.github.com>
2018-03-14 17:21:42 -0700

    Create plot.sh

---
+++ plot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo '0' > /proc/sys/kernel/nmi_watchdog
+instructions=8
+thread=1
+while getopts "i:t:" arg
+do
+case $arg in
+	i)
+		instructions=$OPTARG
+	;;
+	t)
+		thread=$OPTARG
+	;;
+        esac
+done
+/root/pmu-tools/toplev.py -I 1000 -l3 -x, -o tl-build.csv ~/cmpe202_Computer_Architecture/run.sh -i ${instructions} -t ${thread}
+/root/pmu-tools/tl-barplot.py --cpu C0-T0 tl-build.csv -o ~/plot_${instruction}_${thread}.png
